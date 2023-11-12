2a72ce1772eb0cb7b303fd2b4580e17858348415
sumonaung23 <sumonaung575@gmail.com>
2023-11-12 23:23:19 +0630

    modified api-key as it doesn't work with .env

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,8 @@ export const exerciseOptions = {
     method: 'GET',
     params: {limit: '10'},
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        /* 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY, */
+        'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
 };
