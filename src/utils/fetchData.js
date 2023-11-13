038d5ce5bb690c55681501ac0f1e6793fef207ed
sumonaung23 <sumonaung575@gmail.com>
2023-11-13 23:52:09 +0630

    modified exerciseOptions and added YoutubeOptions from RapidAPI

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,12 +1,19 @@
 export const exerciseOptions = {
     method: 'GET',
-    params: {limit: '10'},
     headers: {
         /* 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY, */
         'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
 };
+
+export const youtubeOptions = {
+        method: 'GET',
+        headers: {
+        'X-RapidAPI-Key': 'c8a42e4734mshd8a3fa3018a4094p1ff31cjsndf003e485402',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+        }
+    };
 
 export const fetchData = async(url, options) => {
     const response = await fetch(url, options);
