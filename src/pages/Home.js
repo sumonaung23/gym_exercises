7004f7833afac8db7b3cfd08798fb8876e2985eb
sumonaung23 <sumonaung575@gmail.com>
2023-11-13 23:43:13 +0630

    get rid of console.log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
     const [bodyPart, setBodyPart] = useState('all');
     const [exercises, setExercises] = useState([]);
 
+    console.log(bodyPart);
+
     return (
         <Box>
             <HeroBanner />
