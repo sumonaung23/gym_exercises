b5892bbdd57c04d7abd2a929a44d8a99337511d6
sumonaung23 <sumonaung575@gmail.com>
2023-11-13 23:52:56 +0630

    modified exercise to item and added window.scrollTo

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -23,11 +23,13 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
             const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
 
             const searchExercises = exercisesData.filter(
-                (exercise) => exercise.name.toLowerCase().includes(search) ||
-                exercise.target.toLowerCase().includes(search) ||
-                exercise.equipment.toLowerCase().includes(search) ||
-                exercise.bodyPart.toLowerCase().includes(search)
+                (item) => item.name.toLowerCase().includes(search) ||
+                item.target.toLowerCase().includes(search) ||
+                item.equipment.toLowerCase().includes(search) ||
+                item.bodyPart.toLowerCase().includes(search)
             );
+
+            window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
 
             setSearch('');
             setExercises(searchExercises)
