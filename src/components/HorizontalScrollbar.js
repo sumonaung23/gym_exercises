9de6890c1e62e748f64a7116b2c5ee90e532ab0d
sumonaung23 <sumonaung575@gmail.com>
2023-11-15 00:00:03 +0630

    added isBodyParts props and modified the scrollMenu with conditional Statement

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -6,6 +6,7 @@ import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 import BodyPart from './BodyPart'
+import ExerciseCard from './ExerciseCard'
 
 const LeftArrow = () => {
     const { scrollPrev } = useContext(VisibilityContext);
@@ -27,7 +28,7 @@ const RightArrow = () => {
     );
 };
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
     return (
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {data.map((item) => (
@@ -36,7 +37,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
                 itemId={item.id || item}
                 title={item.id || item}
                 m="0 40px">
-                    <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+                    {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} /> : <ExerciseCard exercise={item} />}
                 </Box>
             ))}
         </ScrollMenu>
